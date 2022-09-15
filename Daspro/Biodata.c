85aa2268f7511b16580ef59ef75799ac7a72b445
Firdaus Youichi <113011077+Firdaus-Youichi@users.noreply.github.com>
2022-09-16 03:45:05 +0700

    Add files via upload

---
+++ Daspro/Biodata.c
@@ -0,0 +1,11 @@
+
+#include<stdio.h>
+int main()
+{
+    printf ("\n");
+    printf ("\tNama           : Firdaus Youichi Yamamoto\n");
+    printf ("\tUmur           : 18\n");
+    printf ("\tJenis Kelamin  : laki-laki\n");
+    printf ("\tAlamat         : Perumahan Ndalem Yudhistira\n");
+
+}
