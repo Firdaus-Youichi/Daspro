2b9fd8f0a56fc2b5854733230b34ab3d03ba1400
Firdaus Youichi <113011077+Firdaus-Youichi@users.noreply.github.com>
2022-10-05 17:24:01 +0700

    Add files via upload

---
+++ Daspro/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //Tipe Data
+    /*
+    string - menuliskan kalimat
+    interger - bilangan bulat
+    floating point - bilangan pecahan
+    double floating point - bilangan pecahan lebih banyak digit
+    char - karakter terbatas - 1 huruf
+    bool - kondisi true dan false
+    */
+
+    string nama_aku = "Fadri Yurianto";
+    int nomor_int = 10;
+    float nomor_float = 10.1;
+    double nomor_double = 10.2;
+    char karakter = 'A';
+    bool bolean = true;
+
+    cout << "nama aku: " << nama_aku << endl;
+    cout << "nomor int: " << nomor_int << endl;
+    cout << "nomor float: " << nomor_float << endl;
+    cout << "nomor double: " << nomor_double << endl;
+    cout << "karakter: " << karakter << endl;
+    cout << "bolean: " << bolean << endl;
+
+
+
+    return 0;
+}
